80323df5ec046ef3c3078046ec78d6cdab70476c
sseunn <onns-sseunn@github.com>
2022-12-21 16:43:29 +0900

    nextjs 초기세팅

---
+++ pages/sub/about.js
@@ -0,0 +1,6 @@
+export default function about() {
+  return <>
+   <h1>/pages/sub/index.js</h1>
+   <a href="/">/pages/index.js</a>
+  </>
+}
---
+++ pages/sub/fetch.js
@@ -0,0 +1,7 @@
+import {useEffect} from 'react'  
+export default function Fetch() {
+  return <>
+   <h1>/pages/sub/index.js</h1>
+   <a href="/">/pages/index.js</a>
+  </>
+}
